5fa23898462d6f650e2184a75b82862daf16dcc9
Charles Bibik <cbibik@bibikstudios.com>
2023-05-16 12:10:14 -0700

    Question 24 | Swap Nodes

---
+++ Challenges_0-100/24-Swap-Nodes-In-Pairs/Bibikski.SwapNodes/Program.cs
@@ -0,0 +1,25 @@
+﻿using Bibikski.SwapNodes;
+
+ListNode node = new ListNode()
+{
+    val = 1,
+    next = new ListNode()
+    {
+        val = 2,
+        next = new ListNode()
+        {
+            val=3,
+            next=new ListNode()
+            {
+                val = 4,
+                next=new ListNode()
+                {
+                    val=5
+                }
+            }
+        }
+    }
+};
+node = null; 
+
+new SwapNodes().SwapPairs(node);
---
+++ Challenges_0-100/24-Swap-Nodes-In-Pairs/Bibikski.SwapNodes/SwapNodes.cs
@@ -0,0 +1,67 @@
+﻿namespace Bibikski.SwapNodes;
+
+public class SwapNodes
+{
+    public ListNode SwapPairs(ListNode head)
+    {
+        ListNode response = new ListNode();
+        if (head == null) return null; 
+        if (head?.next == null && head?.val > 0)
+            return head;
+
+        int ptr = 0;
+        ListNode? ptrNode = head;
+        Dictionary<int, ListNode> nodes = new Dictionary<int, ListNode>(); 
+        
+jmp_nodeLoop:
+        nodes.Add(ptr, ptrNode);
+        if (ptrNode?.next != null)
+        {
+            ptrNode = ptrNode.next;
+            ptr++;
+            goto jmp_nodeLoop;
+        }
+        else
+        {
+            int nodesCount = nodes.Count; //So we do not have to interact with the dictionary more than we have to.
+            for (int i = 0; i < nodesCount; i++)
+            {
+                if ((i+1) % 2 == 0)
+                {
+                    if (i+1 == 2)
+                    {
+                        response = new ListNode()
+                        {
+                            val=nodes[i].val,
+                            next = new ListNode()
+                            {
+                                val = nodes[i-1].val
+                            }
+                        };
+                        ptrNode = response.next;
+                    }
+                    else
+                    {
+                        ptrNode.next = new ListNode()
+                        {
+                            val = nodes[i].val,
+                            next = new ListNode()
+                            {
+                                val = nodes[i - 1].val,
+                            }
+                        };
+                        ptrNode = ptrNode.next.next;
+                    }
+                }
+                else if (i+1 == nodesCount && (i+1) % 2 != 0)
+                {
+                    ptrNode.next = new ListNode()
+                    {
+                        val = nodes[i].val
+                    };
+                }
+            }
+        }
+        return response;
+    } 
+}
