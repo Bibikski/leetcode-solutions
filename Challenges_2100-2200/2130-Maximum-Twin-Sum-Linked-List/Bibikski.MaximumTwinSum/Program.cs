e8eb76d0aa4d6fd99665fc98de2024d48f9e22b0
Charles Bibik <cbibik@bibikstudios.com>
2023-05-17 10:47:50 -0700

    Challenge 2130 | Maximum Twin Sum for a linked list

---
+++ Challenges_2100-2200/2130-Maximum-Twin-Sum-Linked-List/Bibikski.MaximumTwinSum/Program.cs
@@ -0,0 +1,20 @@
+﻿using Bibikski.MaximumTwinSum;
+
+ListNode testNode = new ListNode()
+{
+    val=4,
+    next = new ListNode()
+    {
+        val = 2,
+        next = new ListNode()
+        {
+            val = 2,
+            next = new ListNode()
+            {
+                val=3
+            }
+        }
+    }
+};
+
+new SumUtil().PairSum(testNode);
---
+++ Challenges_2100-2200/2130-Maximum-Twin-Sum-Linked-List/Bibikski.MaximumTwinSum/SumUtil.cs
@@ -0,0 +1,34 @@
+﻿namespace Bibikski.MaximumTwinSum;
+
+public class SumUtil
+{
+    public int PairSum(ListNode head)
+    {
+        int maxSum = 0;
+        int totalElements = 0;
+        int ptr = 0;
+        Dictionary<int, int> nodeValues = new Dictionary<int, int>();
+        ListNode ptrNode = head;
+        
+jmp_nextNode:
+        nodeValues.Add(totalElements, ptrNode.val);
+        totalElements++;
+        if (ptrNode.next != null)
+        {
+            ptrNode = ptrNode.next;
+            goto jmp_nextNode;  
+        }
+
+        if (totalElements % 2 == 0)
+        {
+            int totalResults = totalElements / 2;
+            List<int> sums = new List<int>(totalResults);
+            for (int i = 0; i < totalResults; i++)
+            {
+                sums.Add(nodeValues[i] + nodeValues[(totalElements-1)-i]); 
+            }
+            maxSum = sums?.OrderByDescending(x=>x)?.FirstOrDefault() ?? 0;
+        }
+        return maxSum;
+    } 
+}
